95c9300446fe56b3bce1bdc0aa195fcf82d73fb8
Nivel2gx <nivel2gx@gmail.com>
2018-05-28 13:52:00 -0300

    Initial commit
    Initiala commit

---
+++ src/com/genexus/util/GXSoapHandler.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.genexus.util;
+
+/**
+ *
+ * @author jlarrosa
+ */
+public class GXSoapHandler {
+ public static void setHandlers(Integer remoteHandle, com.genexus.ModelContext context, String serviceName, javax.xml.ws.BindingProvider bProvider)   
+ {
+     System.out.print("Hello setHandlers method");
+ }
+}
